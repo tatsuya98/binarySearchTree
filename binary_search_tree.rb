a09d0f0ae7e1e986a56bd3f104b3fa7364001c40
tatsuya98 <nathanbailey_62@hotmail.com>
2024-02-13 13:45:03 +0000

    wrote insert function

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -16,6 +16,25 @@ class Tree
     node.right = build_tree(array, mid + 1, last)
     node
   end
+
+  def insert(value)
+    node = @root
+    while !node.left.nil? || !node.right.nil?
+      if value == node.data
+        puts "tree already contains a node with the value #{value}"
+        break
+      elsif value > node.data
+        node = node.right
+      else
+        node = node.left
+      end
+    end
+    return nil if value == node.data
+
+    node.left = Node.new(value) if node.data > value
+
+    node.right = Node.new(value)
+  end
 end
 
 class Node
@@ -27,3 +46,7 @@ class Node
     @right = nil
   end
 end
+test = Tree.new
+arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324].uniq.sort
+test.root = test.build_tree(arr, 0, arr.length - 1)
+test.insert(324)
